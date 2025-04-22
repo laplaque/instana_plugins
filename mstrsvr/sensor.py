0376c9ff43ee1a7e727beab6912feffecb151ce6
Earl Plak <5597016+laplaque@users.noreply.github.com>
2025-04-22 22:00:12 +0200

    Updated tag to v0.0.8

--- mstrsvr/sensor.py
+++ mstrsvr/sensor.py
@@ -18,7 +18,7 @@ from common.base_sensor import run_sensor
 # Define the process name with proper capitalization
 PROCESS_NAME = "MSTRSvr"
 PLUGIN_NAME = "com.instana.plugin.python.microstrategy_mstrsvr"
-VERSION = "0.0.7"
+VERSION = "0.0.8"
 
 if __name__ == "__main__":
     run_sensor(PROCESS_NAME, PLUGIN_NAME, VERSION)
